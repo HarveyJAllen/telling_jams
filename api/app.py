5114a0cb088e7226f4dba6e74a35db0443cc73e6
opdeadbush <harvey.allen@abingdon.org.uk>
2023-01-19 08:52:10 +0000

    read

--- api/app.py
+++ api/app.py
@@ -12,6 +12,6 @@ def index():
             outfile.writelines(message+"\n")
         return redirect("/")
     if request.method == "GET":
-        with open("/tmp/text.txt", "r") as file:
+        with open("/tmp/text.txt", "r+") as file:
             messages = file.readlines()
         return render_template("index.html", messages=messages)
