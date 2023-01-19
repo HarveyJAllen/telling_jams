34c8238f4532475de05b5427a594fca3ae9aa88e
opdeadbush <harvey.allen@abingdon.org.uk>
2023-01-19 08:34:05 +0000

    data

--- api/app.py
+++ api/app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         message = request.form.get("message")
-        with open("api/static/messages.txt", "a") as outfile:
+        with open("data/text.txt", "a") as outfile:
             outfile.writelines(message+"\n")
         return redirect("/")
     if request.method == "GET":
-        with open(join(dir, '..', 'data', 'text.txt'), "r+") as file:
+        with open("data/text.txt", "r+") as file:
             messages = file.readlines()
         return render_template("index.html", messages="Hello")
