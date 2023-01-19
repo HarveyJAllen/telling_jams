912da274e6d58d2749429199821307308bf3390c
opdeadbush <harvey.allen@abingdon.org.uk>
2023-01-19 08:54:55 +0000

    append

--- api/app.py
+++ api/app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         message = request.form.get("message")
-        with open("/tmp/text.txt", "a") as outfile:
+        with open("/tmp/text.txt", "a+") as outfile:
             outfile.writelines(message+"\n")
         return redirect("/")
     if request.method == "GET":
-        with open("/tmp/text.txt", "r+") as file:
+        with open("/tmp/text.txt", "a+") as file:
             messages = file.readlines()
         return render_template("index.html", messages=messages)
